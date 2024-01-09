5bee24c34c28b603478946323ca79af4884dce66
aronszocs <aron.szocs.ext@sodexo.com>
2024-01-09 12:22:47 +0100

    cat

---
+++ CatPlugin/Cat.cs
@@ -0,0 +1,17 @@
+﻿namespace CatPlugin
+{
+    public class Cat
+    {
+        public string Name { get; set; } = "Cat";
+
+        public void Add()
+        {
+            Console.WriteLine($"{Name} was added to the Zoo.");
+        }
+
+        public void Sound()
+        {
+            Console.WriteLine($"{Name} goes meow.");
+        }
+    }
+}
